2ef7ff02bc09db7a1c90170d0fde499ead0c8fc8
Stephen Collins <scrivera64@gmail.com>
2020-10-31 23:45:15 -0600

    Header: add some additional properties to the format

--- usr/lib/sticky/note_buffer.py
+++ usr/lib/sticky/note_buffer.py
@@ -8,7 +8,7 @@ TAG_DEFINITIONS = {
     'italic': {'style': Pango.Style.ITALIC},
     'underline': {'underline': Pango.Underline.SINGLE},
     'link': {'underline': Pango.Underline.SINGLE, 'foreground': 'blue'},
-    'header': {'scale': 1.5}
+    'header': {'scale': 1.5, 'weight': 500, 'pixels-above-lines': 14, 'pixels-below-lines': 4}
 }
 
 class GenericAction(object):
