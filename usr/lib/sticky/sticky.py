2f9e433ab12ca0ef27788cac1c79ff955fe83454
scott@murphys.place <30272821+63BeetleSmurf@users.noreply.github.com>
2022-01-31 22:58:04 +0000

    Skip confirm dialog when closing empty note.
    If there is not title or text on a note, assume it is empty and skip confirm dialog when removing.

--- usr/lib/sticky/sticky.py
+++ usr/lib/sticky/sticky.py
@@ -482,7 +482,8 @@ class Note(Gtk.Window):
 
     def remove(self, *args):
         # this is ugly but I'm not sure how to make it look better :)
-        if (self.app.settings.get_boolean('disable-delete-confirm') or
+        if ((not self.title.get_text() and not self.buffer.get_internal_markup()) or
+            self.app.settings.get_boolean('disable-delete-confirm') or
             confirm(_("Delete Note"), _("Are you sure you want to remove this note?"),
                     self, self.app.settings, 'disable-delete-confirm')):
             self.emit('removed')
